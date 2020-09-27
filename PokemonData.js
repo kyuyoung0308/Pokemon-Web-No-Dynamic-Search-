ee2663613f2c81b044010cebc03185918aadf1bb
Kyu Young Han <khan67@uwo.ca>
2020-09-26 23:00:55 -0400

    Added pokemon description arrays and function outlines

--- PokemonData.js
+++ PokemonData.js
@@ -1,8 +1,26 @@
-var pokemons;
-var pokemonName=["Bulbasaur", "Ivysaur"];
-var type =["Grass Poison", "Grass Poison"];
+var namePokemon;
+var pokemonName=["Bulbasaur", "Ivysaur", "Venasaur", "MegaVenasaur", "Charmander", "Charmeleon", "Charizard", "MegaCharizardX", "MegaCharizardY", "Squirtle", "Wartortle", "Blastoise", "MegaBlastoise", "Caterpie", "Metapod", "Butterfree", "Weedle", "Kakuna", "Beedrill", "Pidgey", "Pideotto", "Pidgeot", "Rattata", "Raticate"];
+var type =["Grass Poison", "Grass Poison", "Grass Poison", "Grass Poison", "Fire", "Fire", "Fire Flying", "Fire Dragon", "Fire Flying", "Water", "Water", "Water", "Water",
+"Bug", "Bug", "Bug Flying", "Bug Poison", "Bug Poison", "Bug Poison", "Normal Flying","Normal Flying","Normal Flying", "Normal", "Normal"];
 function searchName() {
-  pokemons = document.getElementById("names").value;
-  //alert("HEllo" + pokemons);
-  alert("pokemon"+ pokemons);
+  var list = [];
+  var counter = 0, counterArray = 0;
+  namePokemon = document.getElementById("names").value;
+
+  while(counter<5 && counterArray <20){
+    if (pokemonName[counterArray].length() >= namePokemon.length()){
+    if (pokemonName[counterArray].includes(namePokemon)){
+
+       // Add pokemonName[counterArray] to list
+       // counter++;
+    }
+  }
+ counterArray++;
+
+  }
+  alert("pokemon"+ namePokemon);
+
+}
+function searchNumber(){
+
 }
