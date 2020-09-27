5e0b317dadc4afee9e9f808335b5de9739290865
Kyu Young Han <khan67@uwo.ca>
2020-09-26 22:03:10 -0400

    Added javascript and html file

---
+++ PokemonData.js
@@ -0,0 +1,8 @@
+var pokemons;
+var pokemonName=["Bulbasaur", "Ivysaur"];
+var type =["Grass Poison", "Grass Poison"];
+function searchName() {
+  pokemons = document.getElementById("names").value;
+  //alert("HEllo" + pokemons);
+  alert("pokemon"+ pokemons);
+}
